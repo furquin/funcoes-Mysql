08fdaa365d3aaf8e5498bc935e065fe9e44c5baf
furquin <laert.ff@gmail.com>
2022-03-26 20:53:12 -0300

    feat: exibe duas colunas sobre musicas  com nome e quantidade de reprodução de acordo com plano

---
+++ desafio10.sql
@@ -0,0 +1,12 @@
+SELECT 
+  musicas.nome_musica  AS 'nome',
+  COUNT(musicas.id) AS 'reproducoes'
+FROM SpotifyClone.musicas
+	JOIN SpotifyClone.historico_reproducoes
+		ON historico_reproducoes.musica_id = musicas.id
+	JOIN SpotifyClone.usuarios
+		ON usuarios.id = historico_reproducoes.usuario_id
+WHERE
+  plano_id IN(1,3)
+GROUP BY nome
+ORDER BY nome;
